e8c6f83abfda677b81a631bbbd0ccc8dca97212b
habnk <habnk@DESKTOP-HVVIL63>
2021-10-02 10:12:23 +0900

    プロジェクト ファイルを追加します。

---
+++ PointApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PointApp.Services;
+using PointApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PointApp
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new AppShell();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ PointApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PointApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PointApp.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
