2b0b18c4ceef4fcb12261ef3aea1dd3de4319a0e
keisato848 <habnk1227@gmail.com>
2022-02-25 20:22:30 +0900

    ソース整理

--- PointApp/AppShell.xaml.cs
+++ PointApp/AppShell.xaml.cs
@@ -9,8 +9,6 @@ namespace PointApp
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
